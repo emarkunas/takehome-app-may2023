e604b3b4f09bc38adf950f03ece72857ca53e2c6
emarkunas <emarkunas@users.noreply.github.com>
2023-05-19 20:31:15 -0500

    Fix linting script and run across app

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
-import './App.css';
+
+// Global Styles
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
+
+// App Styles
+import './App.scss';
 
 import BenefitsDashboard from 'src/pages/BenefitsDashboard';
 
 function App() {
   return (
-    <div className="App">
+    <div className="MyApp">
       <BenefitsDashboard />
     </div>
   );
